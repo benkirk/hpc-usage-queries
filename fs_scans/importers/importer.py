9b198db80c8f652f4278e26add519d796056d9c0
Benjamin Kirk <benkirk@ucar.edu>
2026-02-01 06:50:53 -0700

    consolodate common imports

--- fs_scans/importers/importer.py
+++ fs_scans/importers/importer.py
@@ -25,46 +25,7 @@ Multi-Pass Algorithm:
         - Record scan metadata
 """
 
-import grp
-import multiprocessing as mp
-import os
-import pwd
-import sys
-import time
-from collections import defaultdict
-from datetime import datetime
-from functools import lru_cache
-from pathlib import Path
-from typing import Any, Callable, Generator, TextIO
-
-from rich.progress import TextColumn
-from sqlalchemy import func, insert, text
-from sqlalchemy.dialects.sqlite import insert as sqlite_insert
-
-from ..cli.common import console, create_progress_bar, format_size
-from ..core.database import (
-    extract_filesystem_from_filename,
-    extract_scan_timestamp,
-    get_db_path,
-    get_session,
-    init_db,
-    set_data_dir,
-    drop_tables,
-)
-from ..core.models import (
-    AccessHistogram,
-    Directory,
-    DirectoryStats,
-    GroupInfo,
-    GroupSummary,
-    OwnerSummary,
-    ScanMetadata,
-    SizeHistogram,
-    UserInfo,
-    classify_atime_bucket,
-    classify_size_bucket,
-)
-from ..parsers.base import FilesystemParser
+from .common_imports import *
 from .file_handling import *
 from .pass1 import *
 from .pass2 import *
