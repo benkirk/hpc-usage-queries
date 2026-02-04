b308a870936d0727d83ad17ecae159e7dbe4371d
Benjamin Kirk <benkirk@ucar.edu>
2026-02-04 09:27:24 -0700

    Fix pytest failures after schema refactor
    - Remove  import from
    - Update  to use new  model and foreign keys
    - Remove obsolete  view usage in tests

--- qhist_db/__init__.py
+++ qhist_db/__init__.py
@@ -1,6 +1,6 @@
 """QHist Database - SQLAlchemy ORM for HPC job history data."""
 
-from .database import get_db_path, get_engine, get_session, init_db, create_views, VALID_MACHINES
+from .database import get_db_path, get_engine, get_session, init_db, VALID_MACHINES
 from .models import Job, DailySummary, JobCharged
 from .queries import JobQueries
 
@@ -9,7 +9,6 @@ __all__ = [
     "get_engine",
     "get_session",
     "init_db",
-    "create_views",
     "Job",
     "DailySummary",
     "JobCharged",
